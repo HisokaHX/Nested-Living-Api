cab1daf04177b19ed00c739950b8047acaf3184f
HisokaHX <luca.tobiello92@gmail.comgit config --global user.email luca.tobiello92@gmail.com>
2024-02-21 17:38:30 +0100

    User

---
+++ controllers/auth.controller.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+const { statusCodes, StatusCodes } = require("http-status-codes");
+const User = require("../models/User.model");
+
+module.exports.login = (req, res, next) => {
+    const { email, password } = req.body;
+
+    const LOGIN_ERROR_MESSAGE = 'Email or password invalid';
+
+    const errorFn = () => next(createError(StatusCodes.BAD_REQUEST, LOGIN_ERROR_MESSAGE));
+
+    if (!email || !password) {
+        return errorFn();
+    }
+
+    User.findOne({ email })
+    .then(user => {
+        if (!user) {
+            errorFn();
+        } else {
+            return user.checkPassword(password)
+            .then(match => {
+                if (!match) {
+                    errorFn();
+                } else {
+                    const token = jwt.sign(
+                        { id: user.id },
+                        process.env.JWT_SECRET || 'test',
+                        { expiresIn: '1d' }
+                        )
+
+                        res.json({ accessToken: token });
+                }
+            })
+        }
+    })
+
+    .catch(next)
+}
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,5 +1,21 @@
 const User = require("../models/User.model");
+const { StatusCodes } = require('http-status-codes');
 
 module.exports.create = (req, res, next) => {
-    
+    const userToCreate = {
+        ...req.body
+    }
+
+    User.findOne({ $or: [{ username: req.body.username }, { email: req.body.email }]})
+    .then(user => {
+        if (user) {
+            next(createError(StatusCodes.BAD_REQUEST, 'Username or email already in use'));
+        } else {
+            return User.create(userToCreate)
+            .then(userCreated => {
+                res.status(StatusCodes.CREATED).json(userCreated)
+            })
+        }
+    })
+    .catch(next)
 }
