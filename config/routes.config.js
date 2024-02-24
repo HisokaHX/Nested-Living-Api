bade08bf4e44ff771c8aeb6564d01235d5f0f7fb
HisokaHX <luca.tobiello92@gmail.comgit config --global user.email luca.tobiello92@gmail.com>
2024-02-24 11:21:26 +0100

    register done api

--- config/routes.config.js
+++ config/routes.config.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const usersController = require("../controllers/users.controller")
 const authController = require("../controllers/auth.controller");
 const housesController = require("../controllers/houses.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 router.post('/login', authController.login);
 
-router.post('/register', usersController.create);
+router.post('/users', usersController.create);
+router.get('/users/me', authMiddleware.isAuthenticated, usersController.getCurrentUser);
 
 //House
 router.post('/houses', housesController.createHouse);
